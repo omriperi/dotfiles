decc1281dbf95d1c92321c307cef0fa79a8180e1
dn <dn@dn13-peri.dev.drivenets.net>
2018-10-17 14:31:42 +0300

    Dotfiles: Updated install sh with ctags, vundle & fzf

--- install.sh
+++ install.sh
@@ -12,7 +12,17 @@ retVal=$?
 
 NVIM_LOCATION=/opt/nvim.appimage
 
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 if [ $retVal -ne 0 ]; then
     wget https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage -O $NVIM_LOCATION 
     chmod u+x $NVIM_LOCATION
 fi
+
+# VUNDLE
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# EXUBERANT CTAGS
+sudo apt install exuberant-ctags
